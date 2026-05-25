db1925c403adb4c324119e1010f64ad47a5aaacd
Vitor Gabriel Gomes <vitorggc9@gmail.com>
2026-05-25 19:20:01 -0300

    feat: Adiciona tela padrão para exploração

--- src/main/java/br/edu/ifgoiano/jogo/ui/TelaExploracao.java
+++ src/main/java/br/edu/ifgoiano/jogo/ui/TelaExploracao.java
@@ -3,5 +3,19 @@ package br.edu.ifgoiano.jogo.ui;
 /**
  * Interface grafica da masmorra, exibindo as salas disponiveis para navegacao.
  */
-public class TelaExploracao {
+public class TelaExploracao extends JFrame {
+
+    public TelaExploracao(){
+
+        //Função que pega o tamanho da tela.
+        Dimension tamanhoTela = Toolkit.getDefaultToolkit().getScreenSize();
+        //Define o tamanho do frame para o tamanho capturado da tela.
+        setSize(tamanhoTela.width, tamanhoTela.height);
+        //Definindo que deve ser fechado ao clicar no "X"
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Centraliza
+        setLocationRelativeTo(null);
+
+        
+    }
 }
