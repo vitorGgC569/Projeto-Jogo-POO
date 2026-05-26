dfe4887cfa3ee0a279d74e4181d287e2aaba1127
marco <antoniocosendey@gmail.com>
2026-05-26 18:01:17 -0300

    atualizando3

--- src/main/java/br/edu/ifgoiano/jogo/entidades/EfeitoDanoMultiplicado.java
+++ src/main/java/br/edu/ifgoiano/jogo/entidades/EfeitoDanoMultiplicado.java
@@ -5,5 +5,26 @@ import br.edu.ifgoiano.jogo.interfaces.Efeito;
 /**
  * Efeito responsavel por aplicar um multiplicador ao dano basico baseado no contexto.
  */
-public class EfeitoDanoMultiplicado implements Efeito {
+public class EfeitoDanoMultiplicado extends EfeitoBase implements Efeito {
+    private double multiplicador;
+    private int danoBase;
+
+    public EfeitoDanoMultiplicado(double multiplicador, int danoBase,int dano, String nome, String descricao, int valor, int duracao, boolean permanente) {
+        super(dano,nome,descricao,valor,duracao,permanente);
+        this.multiplicador = multiplicador;
+        this.danoBase = danoBase;
+    }
+
+    public double getMultiplicador() {
+        return multiplicador;
+    }
+    public void setMultiplicador(double multiplicador) {
+        this.multiplicador = multiplicador;
+    }
+    public int getDanoBase() {
+        return danoBase;
+    }
+    public void setDanoBase(int danoBase) {
+        this.danoBase = danoBase;
+    }
 }
--- src/main/java/br/edu/ifgoiano/jogo/entidades/EfeitoEscudo.java
+++ src/main/java/br/edu/ifgoiano/jogo/entidades/EfeitoEscudo.java
@@ -5,5 +5,29 @@ import br.edu.ifgoiano.jogo.interfaces.Efeito;
 /**
  * Efeito responsavel por conceder pontos de escudo temporarios.
  */
-public class EfeitoEscudo implements Efeito {
+
+public class EfeitoEscudo extends EfeitoBase implements Efeito {
+    private int valorEscudo;
+    private int turnosDuracao;
+
+    public EfeitoEscudo(){}
+
+    public EfeitoEscudo(int valorEscudo,int turnosDuracao,int dano, String nome, String descricao, int valor, int duracao, boolean permanente) {
+        super(dano, nome, descricao, valor, duracao, permanente);
+        this.valorEscudo = valorEscudo;
+        this.turnosDuracao = turnosDuracao;
+    }
+
+    public int getValorEscudo() {
+        return valorEscudo;
+    }
+    public void setValorEscudo(int valorEscudo) {
+        this.valorEscudo = valorEscudo;
+    }
+    public int getTurnosDuracao() {
+        return turnosDuracao;
+    }
+    public void setTurnosDuracao(int turnosDuracao) {
+        this.turnosDuracao = turnosDuracao;
+    }
 }
